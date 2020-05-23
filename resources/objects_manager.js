e9fbb6a7bdd87619dce8b41f19d36d11ab1120a2
bonacciog <giovanni.bonaccio96@gmail.com>
2020-05-23 13:58:01 +0200

    from quad to triangle implemented

--- resources/objects_manager.js
+++ resources/objects_manager.js
@@ -1,4 +1,4 @@
-const MAX_TRIANGLES_VERTICES = 3;
+
 
 /**
  * 
@@ -9,9 +9,10 @@ const MAX_TRIANGLES_VERTICES = 3;
  * @param {*} obj_path 
  */
 function addObject(object_name, obj_path, objects) {
+
     if (getMeshByName(objects, object_name) != null)
         return;
-
+    
     var jqXHR = $.ajax({
         async: false,
         url: obj_path,
@@ -26,41 +27,84 @@ function addObject(object_name, obj_path, objects) {
 
     // parse obj from glm_light library
     mesh = ReadOBJ(data, mesh);
-    
+
     // add edge
     mesh = LoadSubdivMesh(mesh);
+
     objects.push({
         name: object_name,
         mesh: mesh
     });
-    
-    return objects;
 
 }
 
 /**
- * Get indices from mesh
+ * Computes triangle indices from mesh
  * @param {*} objects 
  * @param {*} object_name 
  */
-function getIndices(objects, object_name) {
+function getTriangleIndices(objects, object_name) {
     var mesh = getMeshByName(objects, object_name);
 
     if (!mesh)
         return null;
-    
+
     var indices = new Array();
 
-    for (let i = 0; i < mesh.nface; i++)
-        for (let k = 0; k < MAX_TRIANGLES_VERTICES; k++)
-            indices.push(mesh.face[i].vert[k]);          
+    for (let i = 0; i < mesh.nface; i++) {
+        if (verticesNumberOnFace(mesh.face[i]) == 3) {
+            indices.push(mesh.face[i].vert[0]);
+            indices.push(mesh.face[i].vert[1]);
+            indices.push(mesh.face[i].vert[2]);
+        }
+        else if (verticesNumberOnFace(mesh.face[i]) == 4) {
+            indices.push(mesh.face[i].vert[0]);
+            indices.push(mesh.face[i].vert[1]);
+            indices.push(mesh.face[i].vert[2]);
 
+            indices.push(mesh.face[i].vert[2]);
+            indices.push(mesh.face[i].vert[3]);
+            indices.push(mesh.face[i].vert[0]);
+        }
+    }
     return indices;
-
 }
 
 /**
- * Get vertices from mesh
+ * Returns edge from mesh
+ * @param {*} objects 
+ * @param {*} object_name 
+ */
+function getEdges(objects, object_name) {
+    var mesh = getMeshByName(objects, object_name);
+
+    if (!mesh)
+        return null;
+
+    var edges = new Array();
+
+    for (let i = 0; i < mesh.nedge; i++) {
+        edges.push(mesh.edge[i].vert[0]);
+        edges.push(mesh.edge[i].vert[1]);
+    }
+    return edges;
+}
+
+/**
+ * Computes the number of vertex in a given face
+ * @param {*} face 
+ */
+function verticesNumberOnFace(face) {
+    var count = 0;
+    for (let i = 0; i < face.vert.length; i++) {
+        if (face.vert[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+/**
+ * Returns vertices from mesh
  * @param {*} objects 
  * @param {*} object_name 
  */
@@ -76,6 +120,7 @@ function getVertices(objects, object_name) {
         vertices.push(mesh.vert[i].y);
         vertices.push(mesh.vert[i].z);
     }
+
     return vertices;
 }
 
@@ -86,7 +131,40 @@ function getVertices(objects, object_name) {
  */
 function getMeshByName(objects, object_name) {
     for (let i = 0; i < objects.length; i++)
-        if (object_name.localeCompare(objects[i].object_name))
+        if (object_name===objects[i].name)
             return objects[i].mesh;
     return null;
+}
+
+
+/**
+ * Generates an array of random colors
+ * @param {*} vertices 
+ */
+function myMakeRandomVertexColors(vertices) {
+    var colors = new Array();
+    for (let i = 0; i + 3 < vertices.length; i = i + 3) {
+        colors.push(Math.round((Math.random())));
+        colors.push(Math.round((Math.random())));
+        colors.push(Math.round((Math.random())));
+    }
+    return colors;
+}
+
+/**
+ * Generates an array of given colors
+ * @param {*} vertices 
+ * @param {*} r 
+ * @param {*} g 
+ * @param {*} b 
+ */
+function makeGivenVertexColors(vertices, r, g, b) {
+
+    var colors = new Array();
+    for (let i = 0; i + 3 < vertices.length; i = i + 3) {
+        colors.push(r);
+        colors.push(g);
+        colors.push(b);
+    }
+    return colors;
 }
