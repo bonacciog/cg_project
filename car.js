10115385ec703498afcf0ae57ea1a087ad933607
bonacciog <giovanni.bonaccio96@gmail.com>
2020-05-30 11:14:47 +0200

    wheel setted up respect to the chassis. The car is moving up

--- car.js
+++ car.js
@@ -9,7 +9,7 @@ var vx, vy, vz; // velocita' attuale
 
 // queste di solito rimangono costanti
 var velSterzo, velRitornoSterzo, accMax, attrito,
- grip, raggio,
+  grip, raggio,
   attritoX, attritoY, attritoZ; // attriti
 var key;
 
@@ -29,47 +29,47 @@ var key;
 // la struttura controller da DoStep
 function CarDoStep() {
   // computiamo l'evolversi della macchina
- 
+
   var vxm, vym, vzm; // velocita' in spazio macchina
- 
+
   // da vel frame mondo a vel frame macchina
-  var cosf = Math.cos(facing*Math.PI/180.0);
-  var sinf = Math.sin(facing*Math.PI/180.0);
-  vxm = +cosf*vx - sinf*vz;
+  var cosf = Math.cos(facing * Math.PI / 180.0);
+  var sinf = Math.sin(facing * Math.PI / 180.0);
+  vxm = +cosf * vx - sinf * vz;
   vym = vy;
-  vzm = +sinf*vx + cosf*vz;
- 
+  vzm = +sinf * vx + cosf * vz;
+
   // gestione dello sterzo
-  if (key[1]) sterzo+=velSterzo;
-  if (key[3]) sterzo-=velSterzo;
-  sterzo*=velRitornoSterzo; // ritorno a volante fermo
- 
-  if (key[0]) vzm-=accMax; // accelerazione in avanti
-  if (key[2]) vzm+=accMax; // accelerazione indietro
- 
+  if (key[1]) sterzo += velSterzo;
+  if (key[3]) sterzo -= velSterzo;
+  sterzo *= velRitornoSterzo; // ritorno a volante fermo
+
+  if (key[0]) vzm -= accMax; // accelerazione in avanti
+  if (key[2]) vzm += accMax; // accelerazione indietro
+
   // attriti (semplificando)
-  vxm*=attritoX; 
-  vym*=attritoY;
-  vzm*=attritoZ;
+  vxm *= attritoX;
+  vym *= attritoY;
+  vzm *= attritoZ;
 
   // l'orientamento della macchina segue quello dello sterzo
   // (a seconda della velocita' sulla z)
-  facing = facing - (vzm*grip)*sterzo;
- 
+  facing = facing - (vzm * grip) * sterzo;
+
   // rotazione mozzo ruote (a seconda della velocita' sulla z)
   var da ; //delta angolo
-  da=(180.0*vzm)/(Math.PI*raggio) ;
+  da=(180.0*vzm)/(Math.PI*raggio);
   mozzo+=da;
- 
+
   // ritorno a vel coord mondo
-  vx = +cosf*vxm + sinf*vzm;
+  vx = +cosf * vxm + sinf * vzm;
   vy = vym;
-  vz = -sinf*vxm + cosf*vzm;
- 
+  vz = -sinf * vxm + cosf * vzm;
+
   // posizione = posizione + velocita * delta t (ma e' delta t costante)
-  px+=vx;
-  py+=vy;
-  pz+=vz;
+  px += vx;
+  py += vy;
+  pz += vz;
 }
 
 //function drawCube(); // questa e' definita altrove (quick hack)
@@ -88,19 +88,19 @@ function CarDoStep() {
 
 function CarInit() {
   // inizializzo lo stato della macchina
-  px=py=pz=facing=0; // posizione e orientamento
-  mozzo=sterzo=0;   // stato
-  vx=vy=vz=0;      // velocita' attuale
+  px = py = pz = facing = 0; // posizione e orientamento
+  mozzo = sterzo = 0;   // stato
+  vx = vy = vz = 0;      // velocita' attuale
   // inizializzo la struttura di controllo
-  key=[false,false,false,false];
- 
-  velSterzo=3.4;         // A
-//  velSterzo=2.26;       // A
-  velRitornoSterzo=0.93; // B, sterzo massimo = A*B / (1-B)
- 
-  accMax = 0.0011;
-  //accMax = 0.0055;
- 
+  key = [false, false, false, false];
+
+  //velSterzo = 3.4;         // A
+    velSterzo=2.26;       // A
+  velRitornoSterzo = 0.93; // B, sterzo massimo = A*B / (1-B)
+
+  //accMax = 0.0011;
+  accMax = 0.0022;
+
   // attriti: percentuale di velocita' che viene mantenuta
   // 1 = no attrito
   // <<1 = attrito grande
@@ -109,9 +109,9 @@ function CarInit() {
   attritoY = 1.0;  // attrito sulla y nullo
 
   // Nota: vel max = accMax*attritoZ / (1-attritoZ)
-  raggio=0.30
-/*   raggioRuotaA = 0.25;
-  raggioRuotaP = 0.30; */
+  raggio = 0.40
+  /*   raggioRuotaA = 0.25;
+    raggioRuotaP = 0.30; */
 
   grip = 0.45; // quanto il facing macchina si adegua velocemente allo sterzo
 }
@@ -132,57 +132,65 @@ function CarRender() {
 
   //  drawAxis(); // disegno assi spazio mondo
   //  glPushMatrix();
- 
+
   mo_matrix = m4.translate(mo_matrix, px, py, pz);
   mo_matrix = m4.yRotate(mo_matrix, degToRad(facing));
-  
+
   // sono nello spazio MACCHINA
   //drawAxis(); // disegno assi spazio macchina
-/*  gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix);
+  gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix);
   drawObjectFill(objects, 'chassis', 0, 1, 0);
-  drawObjectWire(objects, 'chassis'); */
+  drawObjectWire(objects, 'chassis');
 
+  // ruota posteriore S
+  mo_matrix1 = m4.copy(mo_matrix);
+  // setta la posizione giusta rispetto alla carlinga
+  mo_matrix1 = m4.translate(mo_matrix1, -0.4, -0.6, 0.35);
+
+  mo_matrix1 = m4.translate(mo_matrix1, -0.58, +raggio - 0.28, +0.8);
+  mo_matrix1 = m4.xRotate(mo_matrix1, degToRad(mozzo));
+  gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix1);
+  //  drawCube();
+  drawObjectFill(objects, 'ruota_posteriore', 0, 0, 0);
+  drawObjectWire(objects, 'ruota_posteriore');
 
   // ruota posteriore D
-/*   mo_matrix1 = m4.copy(mo_matrix);
-  //mo_matrix1 = m4.translate(mo_matrix1, 0.58, +raggio - 0.28, +0.8);
-  mo_matrix1 = m4.xRotate(mo_matrix1, degToRad(mozzo));
-  //mo_matrix1 = m4.scale(mo_matrix1, 0.1, raggioRuotaP, raggioRuotaP);
-  gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix1);
-  //  drawCube();
-  drawObjectFill(objects, 'posteriore_destra', 0, 1, 0);
-  drawObjectWire(objects, 'posteriore_destra'); */
-
-   /* // ruota posteriore S
   mo_matrix1 = m4.copy(mo_matrix);
- // mo_matrix1 = m4.translate(mo_matrix1, -0.58, +raggio - 0.28, +0.8);
-  mo_matrix1 = m4.xRotate(mo_matrix1, degToRad(mozzo));
-  //mo_matrix1 = m4.scale(mo_matrix1, 0.1, raggioRuotaP, raggioRuotaP);
-  gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix1);
-  //  drawCube();
-  drawObjectFill(objects, 'posteriore_sinistra', 0, 0, 0);
-  drawObjectWire(objects, 'posteriore_sinistra');
+  // setta la posizione giusta rispetto alla carlinga
+  mo_matrix1 = m4.translate(mo_matrix1, 0.8, -0.35, 0.35)
+  mo_matrix1 = m4.zRotate(mo_matrix1, degToRad(180));
 
-  // ruota anteriore D
-  mo_matrix1 = m4.copy(mo_matrix);
-  //mo_matrix1 = m4.translate(mo_matrix1, 0.58, +raggio - 0.28, -0.55);
-  mo_matrix1 = m4.yRotate(mo_matrix1, degToRad(sterzo));
+  mo_matrix1 = m4.translate(mo_matrix1, -0.58, +raggio - 0.28, +0.8);
   mo_matrix1 = m4.xRotate(mo_matrix1, degToRad(mozzo));
-  //mo_matrix1 = m4.scale(mo_matrix1, 0.08, raggioRuotaA, raggioRuotaA);
   gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix1);
   //  drawCube();
-  drawObjectFill(objects, 'anteriore_destra', 0, 0, 0);
-  drawObjectWire(objects, 'anteriore_destra');*/
+  drawObjectFill(objects, 'ruota_posteriore', 0, 0, 0);
+  drawObjectWire(objects, 'ruota_posteriore');
 
   // ruota anteriore S
   mo_matrix1 = m4.copy(mo_matrix);
-  //mo_matrix1 = m4.translate(mo_matrix1, -0.58, +raggio - 0.28, -0.55);
+  // setta la posizione giusta rispetto alla carlinga
+  mo_matrix1 = m4.translate(mo_matrix1, -0.4, -0.5, -1.4)
+
+  mo_matrix1 = m4.translate(mo_matrix1, -0.58, +raggio - 0.28, -0.55);
   mo_matrix1 = m4.yRotate(mo_matrix1, degToRad(sterzo));
-  
   mo_matrix1 = m4.xRotate(mo_matrix1, degToRad(mozzo));
-  //mo_matrix1 = m4.scale(mo_matrix1, 0.08, raggioRuotaA, raggioRuotaA);
   gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix1);
   //  drawCube();
-  drawObjectFill(objects, 'anteriore_sinistra', 0, 1, 0);
-  drawObjectWire(objects, 'anteriore_sinistra');  
+  drawObjectFill(objects, 'ruota_anteriore', 0, 0, 0);
+  drawObjectWire(objects, 'ruota_anteriore');
+
+  // ruota anteriore D
+  mo_matrix1 = m4.copy(mo_matrix);
+  // setta la posizione giusta rispetto alla carlinga
+  mo_matrix1 = m4.translate(mo_matrix1, 0.8, -0.25, -1.5)
+  mo_matrix1 = m4.zRotate(mo_matrix1, degToRad(180));
+
+  mo_matrix1 = m4.translate(mo_matrix1, -0.58, +raggio - 0.28, -0.55);
+  mo_matrix1 = m4.yRotate(mo_matrix1, degToRad(-sterzo));
+  mo_matrix1 = m4.xRotate(mo_matrix1, degToRad(mozzo));
+  gl.uniformMatrix4fv(_Mmatrix, false, mo_matrix1);
+  //  drawCube();
+  drawObjectFill(objects, 'ruota_anteriore', 0, 0, 0);
+  drawObjectWire(objects, 'ruota_anteriore');
 }
