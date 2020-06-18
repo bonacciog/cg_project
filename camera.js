4f3a4a1d1c9eb21022d259a118015ebe2f902561
bonacciog <giovanni.bonaccio96@gmail.com>
2020-06-18 16:17:05 +0200

    camera implemented

---
+++ camera.js
@@ -0,0 +1,39 @@
+var setCamera = {
+    'default' : setDefaultCamera,
+    'highstatic' : setHighAndStaticCamera,
+    'oncar' : setCameraOnTheCar
+}
+
+function setDefaultCamera(){
+    target = [px, 0, pz-4];  
+                
+    camera = m4.subtractVectors(target, dst, camera)
+
+    cameraMatrix = m4.lookAt(camera, target, up);
+
+    cameraMatrix = m4.yRotate(cameraMatrix, degToRad(facing));
+
+    cameraMatrix = m4.translate(cameraMatrix, 0,3,16);
+}
+
+function setHighAndStaticCamera(){
+
+    target = [px, 0, pz-4];
+
+    cameraMatrix = m4.lookAt(camera, target, up);
+
+    cameraMatrix = m4.translate(cameraMatrix, 0,5,18.5);
+}
+
+
+function setCameraOnTheCar(){
+    target = [px, 0, pz-4];  
+                
+    camera = m4.subtractVectors(target, dst, camera)
+
+    cameraMatrix = m4.lookAt(camera, target, up);
+
+    cameraMatrix = m4.yRotate(cameraMatrix, degToRad(facing));
+
+    cameraMatrix = m4.translate(cameraMatrix, 0,1.5,0.05);
+}
